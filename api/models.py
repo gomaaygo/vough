32cfc78acc02cf22702997e9f0b34521d34d3410
msg <maiurygarcia@gmail.com>
2021-03-28 18:13:49 -0300

    Updated models

--- api/models.py
+++ api/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Organization(models.Model):
-    login = models.CharField(max_length=255, primary_key=True)
-    name = models.CharField(max_length=255)
-    score = models.IntegerField()
+    login = models.CharField(verbose_name='Login', max_length=255, primary_key=True)
+    name = models.CharField(verbose_name='Nome da Organização', max_length=255)
+    score = models.PositiveIntegerField(verbose_name='Score', blank=True, null=True)
+
+    REQUIRED_FIELD = ['login', 'name']
+
+    class Meta:
+        verbose_name = 'organização'
+        verbose_name_plural = 'organizações'
+
+
+    def __str__(self):
+        return self.name
