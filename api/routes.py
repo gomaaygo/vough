0dc78445916b0e17760ca6ccebc45b7b51fb4516
msg <maiurygarcia@gmail.com>
2021-03-28 19:45:30 -0300

    Updated routes.py

--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,14 @@
+# Django imports
 from django.urls import include, path
+
+# Drf imports
 from rest_framework.routers import DefaultRouter
 
-from api import views
+# Api imports 
+from api.views import OrganizationViewSet
+
 
 routers = DefaultRouter()
-routers.register("orgs", views.OrganizationViewSet)
+routers.register(r'orgs', OrganizationViewSet, basename='org')
 
-urlpatterns = [path("", include(routers.urls))]
+urlpatterns = routers.urls
