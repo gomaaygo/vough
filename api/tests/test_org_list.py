6d8ac184bf219078b19492269daf5eb0f0ff888b
msg <maiurygarcia@gmail.com>
2021-03-28 19:49:27 -0300

    Unit tests of the organization listing endpoint

---
+++ api/tests/test_org_list.py
@@ -0,0 +1,53 @@
+import json
+
+# Django imports
+from django.test import TestCase
+
+# Drf imports
+from rest_framework import status
+from rest_framework.test import APIClient
+
+# Api imports 
+from api.models import Organization
+
+
+class TestOrgDeleteApi(TestCase):
+    def setUp(self):
+        self.organization1 = Organization.objects.create(
+            login="instruct-br",
+            name='Instruct',
+            score=39)
+        self.organization1.save()
+        self.organization2 = Organization.objects.create(
+            login="example2",
+            name='Example 2',
+            score=5)
+        self.organization2.save()
+        self.organization3 = Organization.objects.create(
+            login="example3",
+            name='Example 3',
+            score=67)
+        self.organization2.save()          
+        self.resp = self.client.get('/api/orgs/')
+        self.data = [{
+            "login": "example3",
+            "name": "Example 3",
+            "score": 67
+        },
+        {
+            "login": "instruct-br",
+            "name": "Instruct",
+            "score": 39
+        },
+        {
+            "login": "example2",
+            "name": "Example 2",
+            "score": 5
+        }
+        ]
+
+    def test_status_code(self):
+        self.assertEqual(self.resp.status_code, status.HTTP_200_OK)
+
+    def test_response(self):
+        self.assertEqual(json.loads(self.resp.content), self.data)
