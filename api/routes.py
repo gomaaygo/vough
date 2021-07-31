b12bc4c81bc5d991202442cc5e5831e1f31fbfde
msg <maiurygarcia@gmail.com>
2021-07-31 11:37:51 -0300

    Adding documentation to home page

--- api/routes.py
+++ api/routes.py
@@ -5,14 +5,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 # Api imports 
-from api.views import OrganizationViewSet, DocumentationView
+from api.views import OrganizationViewSet
 
 
 routers = DefaultRouter()
 routers.register(r'orgs', OrganizationViewSet, basename='org')
 
-urlpatterns = [
-    path('documentation/', DocumentationView.as_view()),
-]
+urlpatterns = []
 
 urlpatterns += routers.urls
--- vough_backend/urls.py
+++ vough_backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import  DocumentationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.routes"), name="api"),
+    path('', DocumentationView.as_view()),
 ]
