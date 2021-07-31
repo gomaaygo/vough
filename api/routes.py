efb27d3829971b7542a1e35aa1d8b90f4218734a
msg <maiurygarcia@gmail.com>
2021-07-31 00:18:09 -0300

    Documentation added as homepage

--- api/routes.py
+++ api/routes.py
@@ -6,16 +6,12 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views as jwt_views
 
 # Api imports 
-from api.views import OrganizationViewSet, DocumentationView
+from api.views import OrganizationViewSet
 
 
 routers = DefaultRouter()
 routers.register(r'orgs', OrganizationViewSet, basename='org')
 
-urlpatterns = [
-    path('documentation/', DocumentationView.as_view()),
-    path('login/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-]
+urlpatterns = []
 
 urlpatterns += routers.urls
